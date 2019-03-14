95c6f2637ae25f2d19bddab0b93e90fa5bc6dd79
Joanna Yuhas <joannayuhas@gmail.com>
2019-03-14 17:21:49 +0000

    Automatically backed up by Learn

--- lib/while.rb
+++ lib/while.rb
@@ -1,10 +1,8 @@
 def using_while
-	
 	levitation_force = 6
-	
-  	while levatation_force <10
-  	levitation_force += 1 
-  	puts "Wingardium Leviosa"
+	while levitation_force <10
+  levitation_force += 1 
+  puts "Wingardium Leviosa"
   end
 end
 
